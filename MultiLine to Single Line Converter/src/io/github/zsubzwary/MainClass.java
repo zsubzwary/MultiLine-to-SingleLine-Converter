0c530e4e4d934d15488787da475be4c7e6f8556f
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-05-14 09:30:57 +0500

    Created New Project

---
+++ MultiLine to Single Line Converter/src/io/github/zsubzwary/MainClass.java
@@ -0,0 +1,45 @@
+package io.github.zsubzwary;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+
+public class MainClass extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainClass frame = new MainClass();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainClass() {
+		setFont(new Font("Roboto", Font.PLAIN, 14));
+		setTitle("MultiLine to Single Line Converter");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
