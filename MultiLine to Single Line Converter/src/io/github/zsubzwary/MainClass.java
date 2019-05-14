3a24d34fe19a365244054ffb475595693b876ff0
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-05-14 10:19:40 +0500

    Added TextFileds and a Button

--- MultiLine to Single Line Converter/src/io/github/zsubzwary/MainClass.java
+++ MultiLine to Single Line Converter/src/io/github/zsubzwary/MainClass.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainClass extends JFrame {
 
@@ -35,11 +40,34 @@ public class MainClass extends JFrame {
 		setFont(new Font("Roboto", Font.PLAIN, 14));
 		setTitle("MultiLine to Single Line Converter");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 500);
+		setBounds(100, 100, 700, 530);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 664, 225);
+		contentPane.add(scrollPane);
+		
+		JTextArea txtInput = new JTextArea();
+		scrollPane.setViewportView(txtInput);
+		
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 274, 664, 206);
+		contentPane.add(scrollPane_1);
+		
+		JTextArea txtOutput = new JTextArea();
+		scrollPane_1.setViewportView(txtOutput);
+		
+		JButton btnConvert = new JButton("Convert");
+		btnConvert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnConvert.setBounds(585, 243, 89, 23);
+		contentPane.add(btnConvert);
 	}
-
 }
